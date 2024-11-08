a37ff38a78e241c9b0b02e1a07b5f736b45bf9a9
Qerakl <german111474457@gmail.com>
2024-11-08 19:59:14 +0500

    Create Category model

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if(Category::query()->count() === 0) {
+            Category::create([
+                'name' => 'Спорт'
+            ]);
+            Category::create([
+                'name' => 'Одежда'
+            ]);
+            Category::create([
+                'name' => 'Работа'
+            ]);
+            Category::create([
+                'name' => 'Эксасуары'
+            ]);
+        }
+    }
+}
