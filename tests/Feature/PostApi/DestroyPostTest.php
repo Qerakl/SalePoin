1bad1e04d0c7887d9d3bb1eb8e3ffecd124c6647
Qerakl <german111474457@gmail.com>
2024-11-13 21:15:32 +0500

    feat: test DestroyPostTest add  method testDestroyPostNotFound

--- tests/Feature/PostApi/DestroyPostTest.php
+++ tests/Feature/PostApi/DestroyPostTest.php
@@ -51,4 +51,29 @@ class DestroyPostTest extends TestCase
             'id' => $post->id,
         ]);
     }
+
+    public function testDestroyPostNotFound(){
+        //Создаем пользователя-пост и проверяем его в бд
+        $user = User::factory()->create();
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id
+        ]);
+
+        //авторизуем этого пользователя и проверяем на код успеха
+        $loginResponse = $this->post(route('auth.login'), [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+        $loginResponse->assertStatus(200);
+        //Проверяем что такого поста нет
+        $responseDestroyPost = $this->delete(route('post.destroy', '1234' ), [
+            'id' => '1234',
+            'user_id' => $user->id,
+        ]);
+        $responseDestroyPost->assertStatus(404);
+        $this->assertDatabaseMissing('posts', [
+            'id' => '1234',
+        ]);
+    }
+
 }
